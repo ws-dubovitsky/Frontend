47a9f07f554167ba69b6253497d83d0672684112
Oleg Dybovitsky <oleg.dubovitsky@websailors.pro>
2019-02-08 19:44:50 +0300

    update yet

--- src/SharedComponents/utils/axios.js
+++ src/SharedComponents/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const url = "http://localhost:3001/api/weather";
-const url2 = "http://localhost:3001/auth/register"
+// const url2 = "http://localhost:3001/auth/register"
 
 export const WeatherAPIRequest = data => axios.post(url, data);
 
-export const userCreate = data => axios.post(url2, data);
+// export const userCreate = data => axios.post(url2, data);
