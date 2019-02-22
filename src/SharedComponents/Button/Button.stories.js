b585e95f853b31484c5ff1d20ba28b6592c108ce
Oleg Dybovitsky <oleg.dubovitsky@websailors.pro>
2019-02-22 14:29:21 +0300

    gitignore

--- src/SharedComponents/Button/Button.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import MyButton from './index';
-
-storiesOf('MyButton', module)
-  .add('with text', () => (
-    <MyButton onClick={() => { console.log('clicked'); }}>Hello Button</MyButton>
-  ));
